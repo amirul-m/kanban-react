e8df9cbc6da951c928312586fa6305497ffb0048
amirul <muh.amirul.muk@gmail.com>
2018-12-26 19:49:35 +0700

    Updates

---
+++ precache-manifest.64b3c2e6d650e119eff85f6148dfdebd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66b36c9e8a0f016558d4",
+    "url": "/static/js/main.66b36c9e.chunk.js"
+  },
+  {
+    "revision": "8db066e34c854f41c298",
+    "url": "/static/js/1.8db066e3.chunk.js"
+  },
+  {
+    "revision": "66b36c9e8a0f016558d4",
+    "url": "/static/css/main.7aedd031.chunk.css"
+  },
+  {
+    "revision": "c172c0b3ca64833834fd74a9ae114a06",
+    "url": "/index.html"
+  }
+];
